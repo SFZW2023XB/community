ea63e49b1fedf410dc5d7fea020f91a21b96467d
SFZW2023XB <3026847141@qq.com>
2025-02-07 20:09:47 +0800

    add indexcontroller

--- community/src/main/java/life/fuzhong/community/controller/indexController.java
+++ community/src/main/java/life/fuzhong/community/controller/indexController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model ){
-        model.addAttribute("name", name);
-        return "helloPage";
+public class indexController {
+    @GetMapping("/")
+    public String index (){
+
+        return "index";
     }
 
 
