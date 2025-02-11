37d79d7f6a9effca9a85596b8fff9aa9514a17c5
SFZW2023XB <3026847141@qq.com>
2025-02-11 12:45:30 +0800

    extract the github config from code to properties

--- community/src/main/java/life/fuzhong/community/controller/AuthorizeController.java
+++ community/src/main/java/life/fuzhong/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.fuzhong.community.dto.AccessTokenDTO;
 import life.fuzhong.community.dto.GitHubUser;
 import life.fuzhong.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,18 +15,27 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state", required = false) String state){
-        System.out.println("Received code: " + code);
+        //System.out.println("Received code: " + code);
+
 
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
 
-        accessTokenDTO.setClient_id("3771f18ada3a6fd9f46a66910f74fa01348d085957452c6c74552fcaad26b9e7");
+        accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_secret("e3c5a29570bc30113960987dd9598b86e05989cd6723a3f187c98d36ecf5aa48");
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         accessTokenDTO.setGrant_type("authorization_code");
 
