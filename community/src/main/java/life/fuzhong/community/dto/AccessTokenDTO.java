16858c2210fa3d84b32d0e81a6d8a844dd4e82df
SFZW2023XB <3026847141@qq.com>
2025-02-25 22:53:39 +0800

    add the lomboc to add getter and setter

--- community/src/main/java/life/fuzhong/community/dto/AccessTokenDTO.java
+++ community/src/main/java/life/fuzhong/community/dto/AccessTokenDTO.java
@@ -1,5 +1,8 @@
 package life.fuzhong.community.dto;
 
+import lombok.Data;
+
+@Data
 public class AccessTokenDTO {
     String client_id;
     String client_secret;
@@ -8,51 +11,5 @@ public class AccessTokenDTO {
     String state;
     String grant_type;
 
-    public String getGrant_type() {
-        return grant_type;
-    }
 
-    public void setGrant_type(String grant_type) {
-        this.grant_type = grant_type;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getClient_id() {
-        return client_id;
-    }
-
-    public void setClient_id(String client_id) {
-        this.client_id = client_id;
-    }
-
-    public String getClient_secret() {
-        return client_secret;
-    }
-
-    public void setClient_secret(String client_secret) {
-        this.client_secret = client_secret;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getRedirect_uri() {
-        return redirect_uri;
-    }
-
-    public void setRedirect_uri(String redirect_uri) {
-        this.redirect_uri = redirect_uri;
-    }
 }
